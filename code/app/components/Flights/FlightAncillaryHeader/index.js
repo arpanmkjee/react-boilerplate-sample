1dfb583647100532da02cf27ec00fba156c5e9a0
Kanthala Manasa <55280101+manasakanthala17@users.noreply.github.com>
2019-11-11 18:46:34 +0530

    Update index.js

--- code/app/components/Flights/FlightAncillaryHeader/index.js
+++ code/app/components/Flights/FlightAncillaryHeader/index.js
@@ -2,9 +2,33 @@ import React from 'react';
 
 function FlightAncillaryHeader() {
   return (
-    <div>
-    FlightAncillaryHeader
-    </div>
+    <div class="ancillary-header">
+                        <ul class="ancillary-nav">
+                            <li class="">
+                                <span class="ancillary-nav-icon">
+                                    <span class="ancillary-meals-icon"></span>
+                                </span>Meals
+                            </li>
+                            <li class="active">
+                                <span class="ancillary-nav-icon">
+                                    <span class="ancillary-seats-icon"></span>
+                                </span>Seats
+                            </li>
+                            <li class="">
+                                <span class="ancillary-nav-icon">
+                                    <span class="ancillary-baggage-icon"></span>
+                                </span>Baggage
+                            </li>
+                        </ul>
+                        <div class="make_flex ancillary-sector-nav">
+                            <ul class="make_flex">
+                                <li class="active">BOM-HYD</li>
+                            </ul>
+                            <div class="ancillary-flight-summary make_relative">
+                                <p class="cursor_pointer">Flight summary<span class="ancillary-chevron "></span></p>
+                            </div>
+                        </div>
+                    </div>
   );
 }
 
