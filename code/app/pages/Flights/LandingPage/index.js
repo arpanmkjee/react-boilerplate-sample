d860cabc7dea9b3766d12e48afbb952ed49c4c57
koni-a <koni.aruva@gmail.com>
2019-11-12 14:24:47 +0530

    home page changes

--- code/app/pages/Flights/LandingPage/index.js
+++ code/app/pages/Flights/LandingPage/index.js
@@ -10,11 +10,26 @@ import { FormattedMessage } from 'react-intl';
 //import H1 from 'components/H1';
 import messages from './messages';
 
+import Header from '../../../components/App/Header'
+import Footer from '../../../components/App/Footer'
+import LandingSearchWidget from '../../../components/App/LandingSearchWidget'
+
 export default function FlightLandingPage() {
   return (
     <article>
       {/* <H1> */}
-        
+      <div className="bgGradient" style={{ backgroundSize: "100% 485px" }}>
+        <div className="page-section sticky-scroll topSection appendBottom40" id="SW">
+          <div data-cy="landingContainer" className="landingContainer ">
+            <div></div> 
+            <Header></Header>
+            <LandingSearchWidget></LandingSearchWidget>
+          </div>
+          <div className="widgetLoader"></div>
+        </div>
+        <Footer></Footer>
+      </div>  
+  
       {/* </H1> */}
     </article>
   );
